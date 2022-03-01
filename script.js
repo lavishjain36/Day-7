d20415433d0eb61ef5bd5a660cbf91424d55fa1f
lavish <lavishjain36@gmail.com>
2022-03-01 10:58:13 +0530

    Added comments with fun sayhello

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-console.log("Hello Folks");
-console.log(1+2);
-console.log(1+2+3);
+//comments:
+// 1.Taking 2 variable for addition
+// 2.Taking 2 variable for subtraction
+// 3.Taking 2 variable for multiplication
+
+function sayHello() {
+    console.log("Hello World");
+}
+
+sayHello();
